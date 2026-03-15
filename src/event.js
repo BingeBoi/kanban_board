baeb0a22d3277e6cac6cb607f26a299cf383c083
yoihenba <bingeboi5000@gmail.com>
2026-03-15 09:19:09 +0530

    updated1

--- src/event.js
+++ src/event.js
@@ -1,17 +1,49 @@
-import { card_maker } from "./logic.js";
+import { card_maker,inp_ele,outp_ele } from "./logic.js";
+import { display } from "./render.js";
+import { id } from "./state.js";
 
 export function add (addBtn,task,inp,todo){
-    
+    if(inp.value === ""){
+        return;
+    }
     
     addBtn.addEventListener('click',()=>{
         const card_name = inp.value
         task.push({
             "card_name":card_name,
-            "status": "",
-            id:0
+            "status": "todo",
+            "id":id++
         })
+        
         const card = card_maker()
-        todo.appendChild(card)
+        edit_event(card,todo)
+        display(card,todo)
     })
+}
+
+export function edit_event (card,todo){
+
+    card.addEventListener("dblclick",()=>{
+       const  inp_element = inp_ele()
+       const inputs = inp_element.querySelectorAll('input')
+       
+       card.appendChild(inp_element)
+        inputs.forEach(inp => {
+            inp.addEventListener("keydown",(e)=>{
+            if(e.key === "Enter"){
+                const outp = outp_ele(inp_element)             
+                card.replaceChildren(outp)
+                display(todo,card)
+            }
+            
+       })
+       });
+      
+    })
+}
+
+export function start_kanban(addBtn,task,inp,todo){
+    add(addBtn,task,inp,todo)
+    
 }
 
